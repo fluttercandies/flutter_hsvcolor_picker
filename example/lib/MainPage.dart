6d96fd405db4b09a196f771485ccb50ad0c43eca
nohli <43643339+nohli@users.noreply.github.com>
2021-04-08 11:57:35 +0200

    Remove duplicate imports

--- example/lib/MainPage.dart
+++ example/lib/MainPage.dart
@@ -1,7 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter/cupertino.dart";
-import "package:flutter/material.dart";
-import "package:flutter/cupertino.dart";
 import "Pages/SliderPickerPage.dart";
 import "Pages/PalettePickerPage.dart";
 import "Pages/HSVPickerPage.dart";
