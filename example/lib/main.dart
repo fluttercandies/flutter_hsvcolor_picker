d8f0c31937f9e46d8d81a5b1ab0323a67ba29c9d
nohli <43643339+nohli@users.noreply.github.com>
2021-06-25 09:03:52 +0200

    Remove buggy code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,9 @@
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
-import "package:flutter/services.dart";
 import "package:flutter/widgets.dart";
 import "MainPage.dart";
 
-void main() {
-  // Device orientation
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp, //▯
-    DeviceOrientation.landscapeLeft, //▭
-    DeviceOrientation.landscapeRight //▭
-  ]);
-
+void main() async {
   runApp(MyApp());
 }
 
