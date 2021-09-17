31a20601a0890f4f6bbb0c75b84efc3de9b59eb9
nohli <43643339+nohli@users.noreply.github.com>
2021-09-17 19:42:31 +0200

    Make title of sliders look the same everywhere and move to own class

---
+++ lib/src/widgets/slider_title.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SliderTitle extends StatelessWidget {
+  const SliderTitle(
+    this.title,
+    this.text, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 34.0,
+      child: Row(
+        children: <Widget>[
+          Opacity(
+            opacity: 0.65,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      ?.copyWith(fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: Text(
+                  text,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      ?.copyWith(fontSize: 18),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
