bf22af9402c65249a82c6a6d5cda3daa0e5f1b3d
nohli <43643339+nohli@users.noreply.github.com>
2021-09-18 12:22:46 +0200

    Improve UI

--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -273,26 +273,30 @@ class _ColorPickerState extends State<ColorPicker> {
   }
 
   Widget _buildDropdownPortraitMode() {
-    return SizedBox(
-      height: 38,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          border: Border.all(color: Theme.of(context).dividerColor),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(3.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: SizedBox(
+        height: 38,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).dividerColor),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(3.0),
+            ),
           ),
-        ),
-        child: DropdownButton<_IPicker>(
-          iconSize: 32.0,
-          isExpanded: true,
-          isDense: true,
-          style: Theme.of(context).textTheme.headline5?.copyWith(fontSize: 20),
-          value: _pickers[_index],
-          onChanged: (_IPicker? value) => super.setState(
-            () => _pickerOnChanged(value),
+          child: DropdownButton<_IPicker>(
+            iconSize: 32.0,
+            isExpanded: true,
+            isDense: true,
+            style:
+                Theme.of(context).textTheme.headline5?.copyWith(fontSize: 20),
+            value: _pickers[_index],
+            onChanged: (_IPicker? value) => super.setState(
+              () => _pickerOnChanged(value),
+            ),
+            items: _pickers.map(_buildDropdownMenuItems).toList(),
+            underline: const SizedBox(),
           ),
-          items: _pickers.map(_buildDropdownMenuItems).toList(),
-          underline: const SizedBox(),
         ),
       ),
     );
