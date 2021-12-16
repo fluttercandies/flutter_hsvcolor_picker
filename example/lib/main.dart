60a7034458c220704882d238c3ecf8ec856d49eb
nohli <43643339+nohli@users.noreply.github.com>
2021-12-16 13:29:03 +0100

    Remove theme argument

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       home: const MyHomePage(),
     );
