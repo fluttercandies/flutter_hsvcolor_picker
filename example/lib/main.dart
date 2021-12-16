efa8676ffcc670438eb99933931086df624e5ee7
nohli <43643339+nohli@users.noreply.github.com>
2021-12-16 16:43:01 +0100

    Update link to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 
 // For a more detailed example, head over to:
-// https://github.com/fluttercandies/flutter_hsvcolor_picker/example/lib/complex_example
+// https://github.com/fluttercandies/flutter_hsvcolor_picker/tree/master/example/lib/complex_example
 
 void main() => runApp(const MyApp());
 
