bcc11ad9037e910de9416117f341145bafdf4fc4
nohli <43643339+nohli@users.noreply.github.com>
2021-12-16 12:06:53 +0100

    Refactor folder structure, update link

--- example/lib/complex_example/main.dart
+++ example/lib/complex_example/main.dart
@@ -13,6 +13,17 @@ import 'hex_picker_page.dart';
 import 'alpha_picker_page.dart';
 import 'color_picker_page.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MainPage();
+  }
+}
+
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 
 /// For a more detailed example, head over to:
-/// https://github.com/fluttercandies/flutter_hsvcolor_picker/example/lib/complex_example_main.dart
+/// https://github.com/fluttercandies/flutter_hsvcolor_picker/example/lib/complex_example
 
 void main() async {
   runApp(const MyApp());
