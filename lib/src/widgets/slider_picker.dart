1aa52006b0ce77ea00d6b95ad3f513d8c64dccb5
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2022-12-09 16:05:52 +0800

    Update lib/src/widgets/slider_picker.dart

--- lib/src/widgets/slider_picker.dart
+++ lib/src/widgets/slider_picker.dart
@@ -77,7 +77,7 @@ class _SliderPickerState extends State<SliderPicker> {
                 // Color
                 DecoratedBox(
                     decoration: BoxDecoration(
-                      borderRadius: borderRadius,
+                      borderRadius: widget.borderRadius,
                       border: widget.border ?? Border.all(color: Colors.grey),
                       gradient: LinearGradient(colors: widget.colors!),
                     ),
